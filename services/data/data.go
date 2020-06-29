248a130251c88dfd68f2e1ae843ae620f02d4294
sariego <pedro@sariego.dev>
2020-06-29 10:26:50 -0400

    add cached client implementation @ data

--- services/data/data.go
+++ services/data/data.go
@@ -2,15 +2,28 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 	"os"
+	"time"
 
-	// load postgres driver
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
+	"sariego.dev/cotalker-bot/base"
 )
 
 // DB - sql connection pool
 var DB *sql.DB
+
+// CachedClient - uses an internal cache to persist results
+type CachedClient struct {
+	base.Client
+	cache cache
+}
+
+// NewCachedClient - wraps a client with a cache
+func NewCachedClient(delegate base.Client) CachedClient {
+	return CachedClient{delegate, cache{}}
+}
 
 func init() {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
@@ -19,3 +32,42 @@ func init() {
 	}
 	DB = db
 }
+
+// GetChannelInfo - returns channel info, caches it for a day
+func (cc CachedClient) GetChannelInfo(id string) (base.ChannelInfo, error) {
+	cached, err := cc.cache.getChannelInfo(id)
+	if err != nil {
+		expired := err == errExpired
+		fresh, err := cc.Client.GetChannelInfo(id)
+		if err != nil && expired {
+			// fallback to cached
+			return cached, nil
+		}
+		// save to cache and return fresh
+		// todo save to cache
+		return fresh, err
+	}
+	// cache hit!
+	return cached, nil
+}
+
+type cache struct{}
+
+var errExpired = errors.New("cached item expired")
+
+func (cache) getChannelInfo(id string) (base.ChannelInfo, error) {
+	info := base.ChannelInfo{ID: id}
+	var expires time.Time
+
+	err := DB.QueryRow(
+		"select name, users, expires from channel_info where channel_id = $1",
+		id,
+	).Scan(&info.Name, pq.Array(&info.Participants), &expires)
+
+	if err == nil && expires.Before(time.Now()) {
+		err = errExpired
+	}
+
+	return info, err
+}
+
